f910275aebaf4f614787c12c893731618a01f8ee
denisjovic <redzistrejsn@pm.me>
2022-05-11 19:41:00 +0200

    add recuder logic

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useReducer } from "react";
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "increment":
+      return { ...state, count: state.count + 1 };
+    case "decrement":
+      return { ...state, count: state.count - 1 };
+    case "newUserInput":
+      return { ...state, userInput: action.payload };
+    case "toggleColor":
+      return { ...state, color: !state.color };
+    default:
+      throw new Error();
+  }
+};
 
 function App() {
+  const [state, dispatch] = useReducer(reducer, {
+    count: 0,
+    userInput: "",
+    color: false,
+  });
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      style={{
+        backgroundColor: state.color ? "#fff" : "#000",
+        color: state.color ? "#000" : "#fff",
+      }}
+    >
+      <div>{state.count}</div>
+
+      <button onClick={() => dispatch({ type: "increment" })}>+</button>
+      <button onClick={() => dispatch({ type: "decrement" })}>-</button>
+      <button onClick={() => dispatch({ type: "toggleColor" })}>
+        Change Color
+      </button>
+      <input
+        type='text'
+        onChange={(e) =>
+          dispatch({ type: "newUserInput", payload: e.target.value })
+        }
+      />
+      <div>{state.userInput}</div>
     </div>
   );
 }
