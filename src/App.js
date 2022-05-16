95f47f18f2b431debf327f3b56554a5dac973a9e
denisjovic <redzistrejsn@pm.me>
2022-05-16 18:46:53 +0200

    Add some styling

--- src/App.js
+++ src/App.js
@@ -13,27 +13,42 @@ function App() {
       style={{
         backgroundColor: state.color ? "#fff" : "#000",
         color: state.color ? "#000" : "#fff",
-        width: "100%",
-        height: "600px",
       }}
-      className='app'
+      className='app w-auto h-auto m-0 p-0'
     >
-      <div className='p-0 m-0 bg-slate-800	'>
+      <div className='p-0 m-0 bg-slate-800 flex flex-col justify-center items-center	'>
         <div className=''>
-          <h1>{state.count}</h1>
-          <button onClick={() => dispatch({ type: "increment" })}>+</button>
-          <button onClick={() => dispatch({ type: "decrement" })}>-</button>
+          <button
+            className='px-5 py-2 text-white hover:opacity-30 bg-yellow-500'
+            onClick={() => dispatch({ type: "increment" })}
+          >
+            +
+          </button>
+          <button
+            className='px-5 py-2 text-white hover:opacity-30 bg-yellow-500 mx-10'
+            onClick={() => dispatch({ type: "decrement" })}
+          >
+            -
+          </button>
         </div>
-        <button onClick={() => dispatch({ type: "toggleColor" })}>
+        <button
+          className='px-5 py-2 text-white hover:opacity-30'
+          onClick={() => dispatch({ type: "toggleColor" })}
+        >
           Change Color
         </button>
         <input
+          className='text-slate-800'
           type='text'
           onChange={(e) =>
             dispatch({ type: "newUserInput", payload: e.target.value })
           }
         />
-        <h1>{state.userInput}</h1>
+        <h1 className='text center text-white text-7xl p-0 my-10'>
+          {state.count}
+        </h1>
+
+        <h1 className='text-center text-8xl text-white'>{state.userInput}</h1>
       </div>
     </div>
   );
